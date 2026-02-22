2eb13df920d0a9494ff12740099770e33f9b9ad8
김규완 <dubk6298@gmail.com>
2026-02-22 13:37:56 +0900

    공지사항 과제추가 멤버추가 UI 생성 및 컨트롤러 연결

--- src/main/java/com/example/studytrack/controller/StudyDetailController.java
+++ src/main/java/com/example/studytrack/controller/StudyDetailController.java
@@ -7,8 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class StudyDetailController {
 
     @GetMapping("/study/detail")
-    public String studyMain() {
+    public String studyDetail() {
         return "study/studyDetail";
+    }
+
+    @GetMapping("/notice/detail")
+    public String noticeDetail() {
+        return "study/noticeDetail";
+    }
+
+    @GetMapping("/assignment/create")
+    public String assignmentCreate() {
+        return "study/assignmentCreate";
+    }
+
+    @GetMapping("/study/setting")
+    public String studySetting() {
+        return "study/studySetting";
+    }
+
+    @GetMapping("/study/members")
+    public String studyMembers() {
+        return "study/studyMembers";
     }
 
 }
